f8b24ae1dde466e1e54020c57680397684fc763a
Sascha Soerries <32453878+Sorayal@users.noreply.github.com>
2021-03-09 00:18:25 +0100

    Add LeapYear

---
+++ LeapYear/LeapYearCalculation.cs
@@ -0,0 +1,26 @@
+﻿namespace LeapYear
+{
+    static class LeapYearCalculation
+    {
+        public static bool TestIfInputIsLeapYear(int inputYear)
+        {
+            bool isLeapYear;
+            if (inputYear % 4 == 0)
+            {
+                if(inputYear % 100 == 0 && inputYear % 400 != 0)
+                {
+                    isLeapYear = false;
+                }
+                else
+                {
+                    isLeapYear = true;
+                }               
+            }
+            else
+            {
+                isLeapYear = false;
+            }
+            return isLeapYear;
+        }
+    }
+}
---
+++ LeapYear/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool rightInput = false;
+            int actualYear = 0;
+
+            Console.WriteLine("Leap Year Calculation");
+            while (!rightInput)
+            {               
+                Console.WriteLine("Enter year to figure out, if it's a leap year.");
+                string input = Console.ReadLine();
+                rightInput = Int32.TryParse(input, out actualYear);
+                if(rightInput == false)
+                {
+                    Console.WriteLine("Wrong input.");
+                }
+                else if(actualYear >= 1582 == false)
+                {
+                    Console.WriteLine("Input out of range. Lower limit for valid year: 1582");
+                }
+            }
+
+
+            bool IsLeapYear = LeapYearCalculation.TestIfInputIsLeapYear(actualYear);
+            Console.WriteLine($"Is {actualYear} ? : {IsLeapYear}");
+            Console.WriteLine("##############################");
+
+            //List to check
+            List<int> years = new List<int>();
+            for (int i = 1582; i < 2030; i++)
+            {
+                years.Add(i);
+            }
+
+            foreach (var currentYear in years)
+            {
+                IsLeapYear = LeapYearCalculation.TestIfInputIsLeapYear(currentYear);
+                Console.WriteLine($"Is {currentYear} ? : {IsLeapYear}");
+            }          
+        }
+    }
+}
