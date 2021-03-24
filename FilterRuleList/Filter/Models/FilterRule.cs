b915a841c729b5c0a5f01f61eb95928413d1f993
Sascha Soerries <32453878+Sorayal@users.noreply.github.com>
2021-03-25 00:31:11 +0100

    more projects added

---
+++ FilterRuleList/Filter/Models/FilterRule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace Filter.Models
+{
+    public class FilterRule
+    {
+        public int FilterId { get; set; }
+        public string RemoteIpAddress { get; set; }
+        public string FilterPort { get; set; }
+        public string FilterProtocol { get; set; }
+        public string MailRecipient { get; set; }
+    }
+}
---
+++ FilterRuleList/Filter/Program.cs
@@ -0,0 +1,28 @@
+﻿using Filter.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Filter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FilterRule filterRuleOne = new FilterRule
+            {
+                RemoteIpAddress = "192.168.48.10:400;192.168.48.11:50"
+            };
+
+            List<string> RemoteIpAddresses = new List<string>();
+            RemoteIpAddresses.AddRange(filterRuleOne.RemoteIpAddress.Split(';'));
+
+            foreach (var RemoteIPAddress in RemoteIpAddresses)
+            {
+                Console.WriteLine(RemoteIPAddress.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
