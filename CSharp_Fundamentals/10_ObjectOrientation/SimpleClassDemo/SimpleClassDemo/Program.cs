8926b7c9039147392c1af6b83cfb612934b2135a
Sascha Soerries <32453878+Sorayal@users.noreply.github.com>
2021-04-21 19:01:48 +0200

    SimpleClassDemo uploaded

---
+++ CSharp_Fundamentals/10_ObjectOrientation/SimpleClassDemo/SimpleClassDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SimpleClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Creating a new object / instance of the Airplane Class
+            // Airplane is the datatype and fighter the name of this instance.
+            Airplane fighter = new Airplane();
+
+            // Sets the attribute for the instance fighter.
+            fighter.Name = "Eurofighter";
+
+            // It calls the method of this instance
+            fighter.Fly();
+
+            // Class is the prototype and can be used to create many objects / instances.
+            // There is a distinction between class and instance methods and fields.
+            // class methods and fields are directly bound to the class. For example there is 
+            // a class field which is meant to be a common value which every instance shares.
+            // On the opposite side an instance field is directly bound to the concrete object 
+            // and will be not shared by other instances of the same class.
+        }
+    }
+
+    // public is the accessor which indicates how a class is reachable from other classes
+    // Airplane is the Classname and a new datatype within this program. 
+    public class Airplane
+    {
+        // public fields which describe the attributes
+        public string Name;
+
+        // public method, reachable from outside this Airplane class
+        // void means, this method gives no return value. string Name is
+        // the parameter of this method which will be given in
+        public void Fly()
+        {
+            Console.WriteLine($"{Name} wants to fly high");
+        }
+    }
+}
---
+++ CSharp_Fundamentals/1_Strings/EqualsVs==/EqualsVs==/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace EqualsVs__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstString = "hello";
+            string secondString = "hello";
+            string thirdString = null; //= "4";
+
+            // Mixed check value and reference equality check
+            // Value Check for strings is case sensitive
+            Console.WriteLine($"firstString: {firstString}");
+            Console.WriteLine($"secondString: {secondString}");
+            Console.WriteLine($"thirdString: {thirdString}");
+            Console.WriteLine(new string('*', 20));
+            Console.WriteLine($"firstString == secondString: {firstString == secondString}");
+            Console.WriteLine($"firstString equals secondString: {firstString.Equals(secondString)}");
+            Console.WriteLine($"firstString ReferenceEquals secondString: {ReferenceEquals(firstString, secondString)}");
+            Console.WriteLine(new string('*',20));
+            Console.WriteLine($"firstString == thirdString: {firstString == thirdString}");
+            Console.WriteLine($"firstString equals thirdString: {firstString.Equals(thirdString)}");
+            Console.WriteLine($"firstString ReferenceEquals thirdString: {ReferenceEquals(firstString, thirdString)}");
+            Console.WriteLine(new string('*', 20));
+            Console.WriteLine(new string('*', 20));
+            // Seems no difference between == and equals
+
+            // The code below checks for value equality
+            int firstNumber = 6;
+            int secondNumber = 6;
+            int thirdNumber = 7;
+            Console.WriteLine($"firstNumber: {firstNumber}");
+            Console.WriteLine($"secondNumber: {secondNumber}");
+            Console.WriteLine($"thirdNumber: {thirdNumber}");
+            Console.WriteLine(new string('*', 20));
+            Console.WriteLine($"firstNumber == secondNumber: {firstNumber == secondNumber}");
+            Console.WriteLine($"firstNumber equals secondNumber: { firstNumber.Equals(secondNumber)}");
+            Console.WriteLine(new string('*', 20));
+            Console.WriteLine($"firstNumber == thirdNumber: {firstNumber == thirdNumber}");
+            Console.WriteLine($"firstNumber equals thirdNumber: { firstNumber.Equals(thirdNumber)}");
+
+            
+
+            Console.WriteLine(new string('*', 20));
+            Car Saab = new Car();
+            Tank Leopard = new Tank();
+            Tank Merkava = new Tank();
+
+            Console.WriteLine($"Car Saab ReferenceEquals Tank Leopard: {ReferenceEquals(Saab,Leopard)}");
+            Console.WriteLine($"Car Saab equals Tank Leopard: { Saab.Equals(Leopard)}");
+            Console.WriteLine(new string('*', 20));
+            Console.WriteLine($"Tank Merkava ReferenceEquals Tank Leopard: {ReferenceEquals(Merkava, Leopard)}");
+            Console.WriteLine($"Tank Merkava equals Tank Leopard: { Merkava.Equals(Leopard)}");
+            Console.WriteLine(new string('*', 20));
+            Console.WriteLine("Reference changed: Merkava = Leopard");
+            Merkava = Leopard;
+            Console.WriteLine($"Tank Merkava ReferenceEquals Tank Leopard: {ReferenceEquals(Merkava, Leopard)}");
+            Console.WriteLine($"Tank Merkava equals Tank Leopard: { Merkava.Equals(Leopard)}");
+        }
+    }
+
+    interface Vehicle
+    {
+        public string GetSize();
+    }
+
+    public class Car : Vehicle
+    {
+        public string GetSize()
+        {
+            return "Small";
+        }
+    }
+
+    public class Tank : Vehicle
+    {
+        public bool CombatReady { get; set; } = true;
+        public string GetSize()
+        {
+            return "Big";
+        }
+    }
+}
