db0b3138e9e091d540aa24ed2e12ca60556adbed
Sascha Soerries <32453878+Sorayal@users.noreply.github.com>
2021-04-24 14:08:14 +0200

    two little programs added at fundamentals

---
+++ CSharp_Fundamentals/3_Loops/ASCIITable/ASCIITable/Program.cs
@@ -0,0 +1,18 @@
+﻿// For loop to print the 7 bit ASCII table
+using System;
+using System.Text;
+
+namespace ASCIITable
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.OutputEncoding = Encoding.ASCII;
+            for (int i = 0; i < 128; i++)
+            {
+                Console.WriteLine($"Char {(char)i} | Decimal Code {i} | Hex Code {i:X} | Binary {Convert.ToString(i, 2)} ");
+            }
+        }
+    }
+}
---
+++ CSharp_Fundamentals/3_Loops/CalculateForLoop/CalculateForLoop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// Count to from 1 to 100 witout using explicit numbers
+namespace CalculateForLoop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int start = 'a' / 'a'; //ASCII Code 97 => 97/97 == 1
+            int limit = '(' + '<'; //ASCII Code Dec 40 + 60
+            //string end = "....."; //can be used in combination with String.Length
+            for (int i = start; i <= limit; i++)
+            {
+                Console.Write($" {i} ");
+            }
+        }
+    }
+}
